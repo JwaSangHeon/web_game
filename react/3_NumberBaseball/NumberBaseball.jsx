487c60a92e6bec1e984a51b794cc3def862da4da
JwaSangHeon <whk4051@naver.com>
2022-06-09 23:56:21 +0900

    숫자야구 완성

--- react/3_NumberBaseball/NumberBaseball.jsx
+++ react/3_NumberBaseball/NumberBaseball.jsx
@@ -1,36 +1,56 @@
 import React, { useState, useEffect } from "react";
 
+const getNumber = () => {
+  const numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let newarray = [];
+  for (let i = 0; i < 4; i++) {
+    const index = Math.floor(Math.random() * numberList.length);
+    newarray.push(numberList.splice(index, 1));
+  }
+  return newarray.join("");
+};
+
 const NumberBaseball = () => {
   const [value, setValue] = useState("");
   const [results, setResults] = useState([]);
-  const [strike, setStrike] = useState(0);
-  const [ball, setBall] = useState(0);
-  const [start, setStart] = useState(false);
 
-  const number = "8213"; //숫자 4개 이후에 바뀌는것 구현
+  const [number, setNumber] = useState(getNumber()); //숫자 4개 이후에 바뀌는것 구현
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
-  const printResult = () => {};
-  useEffect(() => {
-    printResult();
-  }, [strike, ball]);
-
   const onSubmit = (e) => {
     e.preventDefault();
-    let strikeCount = 0;
-    let ballCount = 0;
-    for (let i = 0; i < 4; i++) {
-      if (number[i] === value[i]) {
-        strikeCount++;
-      } else if (number.includes(value[i])) {
-        ballCount++;
+    if (results.length < 10) {
+      let strike = 0;
+      let ball = 0;
+      let answer = "";
+      for (let i = 0; i < 4; i++) {
+        if (number[i] === value[i]) {
+          strike++;
+        } else if (number.includes(value[i])) {
+          ball++;
+        }
       }
+      if (strike === 4) {
+        answer = `${value} -> 정답!`;
+        setResults((prev) => [...prev, answer]);
+        setValue("");
+        alert(`${number}정답입니다! 게임을 다시 시작합니다.`);
+        setNumber(getNumber());
+        setResults([]);
+      } else {
+        answer = `${value} -> ${strike}스트라이크, ${ball}볼`;
+        setResults((prev) => [...prev, answer]);
+        setValue("");
+      }
+    } else {
+      alert(`10번을 초과했습니다. 정답은${number}`);
+      setValue("");
+      setNumber(getNumber());
+      setResults([]);
     }
-    setStrike(strikeCount);
-    setBall(ballCount);
   };
 
   return (
