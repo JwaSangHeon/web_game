2a2fc8ffca47889a9b86ba2a4b1ed41adec19675
JwaSangHeon <whk4051@naver.com>
2022-06-13 17:45:20 +0900

    반응속도테스트 webpack세팅

---
+++ react/4_ReactionRate/ReactionRate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactionRate = () => {
+  return <h1>hi</h1>;
+};
+
+export default ReactionRate;
---
+++ react/4_ReactionRate/client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReactionRate from "./ReactionRate";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<ReactionRate />);
